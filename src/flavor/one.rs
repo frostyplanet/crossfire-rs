177df4d73e48c761935cc1d45e6976738d251e64
plan <frostyplanet@gmail.com>
2026-01-01 09:18:58 +0800

    OneSize: Add CLOSED_BIT and fix msg drop

--- src/flavor/one.rs
+++ src/flavor/one.rs
@@ -6,60 +6,17 @@ use core::mem::MaybeUninit;
 use crossbeam_utils::CachePadded;
 use std::ptr;
 use std::sync::atomic::{
-    AtomicU16, AtomicU32,
+    AtomicU32, AtomicU8,
     Ordering::{self, Acquire, Relaxed, Release, SeqCst},
 };
 
-struct Slot<T> {
-    value: UnsafeCell<MaybeUninit<T>>,
-    stamp: AtomicU16,
-}
-
-impl<T> Slot<T> {
-    #[inline]
-    fn init(i: u16) -> Self {
-        Self { value: UnsafeCell::new(MaybeUninit::uninit()), stamp: AtomicU16::new(i) }
-    }
-
-    #[inline(always)]
-    fn write(&self, tail: u16, value: *const T) {
-        let mut stamp = self.stamp.load(Acquire);
-        if stamp != tail {
-            let mut backoff = Backoff::new(BackoffConfig::default());
-            loop {
-                backoff.spin();
-                stamp = self.stamp.load(Acquire);
-                if stamp == tail {
-                    break;
-                }
-            }
-        }
-        unsafe { (*self.value.get()).write(ptr::read(value)) };
-        self.stamp.store(tail.wrapping_add(1), Release);
-    }
-
-    #[inline(always)]
-    fn read(&self, head: u16) -> T {
-        let mut stamp = self.stamp.load(Acquire);
-        if stamp != head {
-            let mut backoff = Backoff::new(BackoffConfig::default());
-            loop {
-                backoff.spin();
-                stamp = self.stamp.load(Acquire);
-                if stamp == head {
-                    break;
-                }
-            }
-        }
-        let msg = unsafe { self.value.get().read().assume_init() };
-        // there might be slow reader, update the stamp to allow writer reuse the slot
-        self.stamp.store(head.wrapping_add(1), Release);
-        msg
-    }
-}
+const CLOSED_BIT: u32 = 0x1;
 
 /// A simplify ArrayQueue specialized for size=1
 pub struct OneSize<T> {
+    /// bit 0 , IS_CLOSED marker
+    /// bit 1-9, tail,
+    /// bit 16 ~ 16+7, head
     pos: CachePadded<AtomicU32>,
 
     /// The value in this slot.
@@ -76,21 +33,26 @@ impl<T> OneSize<T> {
     }
 
     #[inline(always)]
-    fn unpack(pos: u32) -> (u16, u16) {
-        let head = (pos >> 16) as u16;
-        let tail = pos as u16;
+    fn unpack(pos: u32) -> (u8, u8) {
+        let head = (pos >> 16) as u8;
+        let tail = (pos >> 1) as u8;
         (head, tail)
     }
 
     #[inline(always)]
-    fn pack(head: u16, tail: u16) -> u32 {
-        ((head as u32) << 16) | (tail as u32)
+    fn pack(head: u8, tail: u8) -> u32 {
+        ((head as u32) << 16) | ((tail as u32) << 1)
     }
 
     /// return Ok(true) on ok, Ok(false) on full, Err(()) to spin
     #[inline(always)]
-    unsafe fn try_push(&self, mut pos: u32, value: *const T, failure: Ordering) -> Result<(), ()> {
+    unsafe fn try_push(
+        &self, mut pos: u32, value: *const T, failure: Ordering,
+    ) -> Result<(), TrySendErr> {
         loop {
+            if pos & CLOSED_BIT > 0 {
+                return Err(TrySendErr::Disconnected);
+            }
             let (head, tail) = Self::unpack(pos);
             if head == tail {
                 let new_pos = Self::pack(head, tail.wrapping_add(1));
@@ -105,32 +67,57 @@ impl<T> OneSize<T> {
                     }
                 }
             } else {
-                return Err(());
+                return Err(TrySendErr::Full);
             }
         }
     }
+}
+
+struct Slot<T> {
+    value: UnsafeCell<MaybeUninit<T>>,
+    stamp: AtomicU8,
+}
+
+impl<T> Slot<T> {
+    #[inline]
+    fn init(i: u8) -> Self {
+        Self { value: UnsafeCell::new(MaybeUninit::uninit()), stamp: AtomicU8::new(i) }
+    }
 
     #[inline(always)]
-    fn pop(&self) -> Option<T> {
-        let mut pos = self.pos.load(Acquire);
-        loop {
-            let (head, tail) = Self::unpack(pos);
-            if head == tail {
-                return None;
-            }
-            debug_assert!(head < tail, "head {} tail {}", head, tail);
-            let next_head = head.wrapping_add(1);
-            let new_pos = Self::pack(next_head, tail);
-            match self.pos.compare_exchange_weak(pos, new_pos, SeqCst, Acquire) {
-                Err(_pos) => {
-                    pos = _pos;
-                }
-                Ok(_) => {
-                    let index = head & 0x1;
-                    return Some(self.slots[index as usize].read(next_head));
+    fn write(&self, tail: u8, value: *const T) {
+        let mut stamp = self.stamp.load(Acquire);
+        if stamp != tail {
+            let mut backoff = Backoff::new(BackoffConfig::default());
+            loop {
+                backoff.spin();
+                stamp = self.stamp.load(Acquire);
+                if stamp == tail {
+                    break;
                 }
             }
         }
+        unsafe { (*self.value.get()).write(ptr::read(value)) };
+        self.stamp.store(tail.wrapping_add(1), Release);
+    }
+
+    #[inline(always)]
+    fn read(&self, head: u8) -> T {
+        let mut stamp = self.stamp.load(Acquire);
+        if stamp != head {
+            let mut backoff = Backoff::new(BackoffConfig::default());
+            loop {
+                backoff.spin();
+                stamp = self.stamp.load(Acquire);
+                if stamp == head {
+                    break;
+                }
+            }
+        }
+        let msg = unsafe { self.value.get().read().assume_init() };
+        // there might be slow reader, update the stamp to allow writer reuse the slot
+        self.stamp.store(head.wrapping_add(1), Release);
+        msg
     }
 }
 
@@ -165,28 +152,50 @@ impl<T> FlavorImpl<T> for OneSize<T> {
 
     #[inline(always)]
     fn try_send(&self, item: &MaybeUninit<T>) -> Result<(), TrySendErr> {
-        todo!();
-        //        // Will always double-check with is_full or try_send_oneshot()
-        //        let pos = self.pos.load(Relaxed);
-        //        unsafe { self.try_push(pos, item.as_ptr(), Relaxed).is_ok() }
+        // Will always double-check with is_full or try_send_oneshot()
+        let pos = self.pos.load(Relaxed);
+        unsafe { self.try_push(pos, item.as_ptr(), Relaxed) }
     }
 
     #[inline(always)]
-    fn try_send_oneshot(&self, _item: *const T) -> Option<Result<(), TrySendErr>> {
-        todo!();
-        //        let pos = self.pos.load(SeqCst);
-        //        Some(unsafe { self.try_push(pos, item, SeqCst).is_ok() })
+    fn try_send_oneshot(&self, item: *const T) -> Option<Result<(), TrySendErr>> {
+        let pos = self.pos.load(SeqCst);
+        Some(unsafe { self.try_push(pos, item, SeqCst) })
     }
 
     #[inline(always)]
     fn try_recv(&self) -> Result<T, TryRecvError> {
-        todo!();
-        //        self.pop()
+        let mut pos = self.pos.load(Acquire);
+        loop {
+            let (head, tail) = Self::unpack(pos);
+            if head == tail {
+                if pos & CLOSED_BIT > 0 {
+                    return Err(TryRecvError::Disconnected);
+                }
+                return Err(TryRecvError::Empty);
+            }
+            debug_assert!(head < tail, "head {} tail {}", head, tail);
+            let next_head = head.wrapping_add(1);
+            let mut new_pos = Self::pack(next_head, tail);
+            if pos & CLOSED_BIT > 0 {
+                new_pos |= CLOSED_BIT;
+            }
+            match self.pos.compare_exchange_weak(pos, new_pos, SeqCst, Acquire) {
+                Err(_pos) => {
+                    pos = _pos;
+                }
+                Ok(_) => {
+                    let index = head & 0x1;
+                    return Ok(self.slots[index as usize].read(next_head));
+                }
+            }
+        }
     }
 
     #[inline]
     fn close(&self) -> bool {
-        todo!();
+        let old = self.pos.fetch_or(CLOSED_BIT, Ordering::SeqCst);
+        old & CLOSED_BIT == 0
     }
 
     #[inline]
@@ -222,5 +231,11 @@ impl<T> FlavorPrivate<T> for OneSize<T> {
         } else {
             RegistryRecv::new_single()
         }
+    }
+}
+
+impl<T> Drop for OneSize<T> {
+    fn drop(&mut self) {
+        let _ = self.try_recv();
     }
 }
