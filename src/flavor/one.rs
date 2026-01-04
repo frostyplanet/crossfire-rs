31ca12883c2a6684bd3fb65980a3aa5928216e63
plan <frostyplanet@gmail.com>
2026-01-04 09:24:05 +0800

    OneSize: Fix is_full and optimise need_drop

--- src/flavor/one.rs
+++ src/flavor/one.rs
@@ -2,13 +2,93 @@ use super::{Flavor, FlavorImpl, FlavorPrivate};
 use crate::backoff::*;
 use crate::waker_registry::*;
 use core::cell::UnsafeCell;
-use core::mem::MaybeUninit;
-use crossbeam_utils::CachePadded;
-use std::ptr;
-use std::sync::atomic::{
-    AtomicU16, AtomicU32,
+use core::mem::{needs_drop, MaybeUninit};
+use core::ptr;
+use core::sync::atomic::{
+    compiler_fence, AtomicU16, AtomicU32,
     Ordering::{self, Acquire, Relaxed, Release, SeqCst},
 };
+use crossbeam_utils::CachePadded;
+
+/// A simplify ArrayQueue specialized for size=1
+pub struct OneSize<T> {
+    pos: CachePadded<AtomicU32>,
+
+    /// The value in this slot.
+    slots: [Slot<T>; 2],
+}
+
+unsafe impl<T: Send> Sync for OneSize<T> {}
+unsafe impl<T: Send> Send for OneSize<T> {}
+
+impl<T> OneSize<T> {
+    #[inline]
+    pub fn new() -> Self {
+        Self { pos: CachePadded::new(AtomicU32::new(0)), slots: [Slot::init(0), Slot::init(1)] }
+    }
+
+    #[inline(always)]
+    fn unpack(pos: u32) -> (u16, u16) {
+        let head = (pos >> 16) as u16;
+        let tail = pos as u16;
+        (head, tail)
+    }
+
+    #[inline(always)]
+    fn pack(head: u16, tail: u16) -> u32 {
+        ((head as u32) << 16) | (tail as u32)
+    }
+
+    /// return Ok(true) on ok, Ok(false) on full, Err(()) to spin
+    #[inline(always)]
+    unsafe fn _try_push(
+        &self, order: Ordering, value: *const T, failure: Ordering,
+    ) -> Result<(), ()> {
+        let mut pos = self.pos.load(order);
+        compiler_fence(Acquire);
+        loop {
+            let (head, tail) = Self::unpack(pos);
+            if head == tail {
+                let new_pos = Self::pack(head, tail.wrapping_add(1));
+                match self.pos.compare_exchange_weak(pos, new_pos, SeqCst, failure) {
+                    Ok(_) => {
+                        let index = tail & 0x1;
+                        self.slots[index as usize].write(tail, value);
+                        return Ok(());
+                    }
+                    Err(_pos) => {
+                        pos = _pos;
+                    }
+                }
+            } else {
+                return Err(());
+            }
+        }
+    }
+
+    #[inline(always)]
+    fn _pop(&self, order: Ordering) -> Option<T> {
+        let mut pos = self.pos.load(order);
+        compiler_fence(Acquire);
+        loop {
+            let (head, tail) = Self::unpack(pos);
+            if head == tail {
+                return None;
+            }
+            let next_head = head.wrapping_add(1);
+            let new_pos = Self::pack(next_head, tail);
+            match self.pos.compare_exchange_weak(pos, new_pos, SeqCst, Acquire) {
+                Err(_pos) => {
+                    pos = _pos;
+                }
+                Ok(_) => {
+                    let index = head & 0x1;
+                    return Some(self.slots[index as usize].read(next_head));
+                }
+            }
+        }
+    }
+}
 
 struct Slot<T> {
     value: UnsafeCell<MaybeUninit<T>>,
@@ -56,86 +136,23 @@ impl<T> Slot<T> {
         self.stamp.store(head.wrapping_add(1), Release);
         msg
     }
-}
-
-/// A simplify ArrayQueue specialized for size=1
-pub struct OneSize<T> {
-    pos: CachePadded<AtomicU32>,
-
-    /// The value in this slot.
-    slots: [Slot<T>; 2],
-}
-
-unsafe impl<T: Send> Sync for OneSize<T> {}
-unsafe impl<T: Send> Send for OneSize<T> {}
-
-impl<T> OneSize<T> {
-    #[inline]
-    pub fn new() -> Self {
-        Self { pos: CachePadded::new(AtomicU32::new(0)), slots: [Slot::init(0), Slot::init(1)] }
-    }
 
     #[inline(always)]
-    fn unpack(pos: u32) -> (u16, u16) {
-        let head = (pos >> 16) as u16;
-        let tail = pos as u16;
-        (head, tail)
-    }
-
-    #[inline(always)]
-    fn pack(head: u16, tail: u16) -> u32 {
-        ((head as u32) << 16) | (tail as u32)
-    }
-
-    /// return Ok(true) on ok, Ok(false) on full, Err(()) to spin
-    #[inline(always)]
-    unsafe fn try_push(&self, mut pos: u32, value: *const T, failure: Ordering) -> Result<(), ()> {
-        loop {
-            let (head, tail) = Self::unpack(pos);
-            if head == tail {
-                let new_pos = Self::pack(head, tail.wrapping_add(1));
-                match self.pos.compare_exchange_weak(pos, new_pos, SeqCst, failure) {
-                    Ok(_) => {
-                        let index = tail & 0x1;
-                        self.slots[index as usize].write(tail, value);
-                        return Ok(());
-                    }
-                    Err(_pos) => {
-                        pos = _pos;
-                    }
-                }
-            } else {
-                return Err(());
-            }
-        }
-    }
-
-    #[inline(always)]
-    fn pop(&self) -> Option<T> {
-        let mut pos = self.pos.load(Acquire);
-        loop {
-            let (head, tail) = Self::unpack(pos);
-            if head == tail {
-                return None;
-            }
-            let next_head = head.wrapping_add(1);
-            let new_pos = Self::pack(next_head, tail);
-            match self.pos.compare_exchange_weak(pos, new_pos, SeqCst, Acquire) {
-                Err(_pos) => {
-                    pos = _pos;
-                }
-                Ok(_) => {
-                    let index = head & 0x1;
-                    return Some(self.slots[index as usize].read(next_head));
-                }
-            }
-        }
+    fn drop(&self) {
+        unsafe { self.value.get().read().assume_init_drop() };
     }
 }
 
 impl<T> Drop for OneSize<T> {
     fn drop(&mut self) {
-        let _ = self.try_recv();
+        if needs_drop::<T>() {
+            let pos = *self.pos.get_mut();
+            let (head, tail) = Self::unpack(pos);
+            if head != tail {
+                let index = head & 0x1;
+                self.slots[index as usize].drop();
+            }
+        }
     }
 }
 
@@ -156,9 +173,7 @@ impl<T> FlavorImpl<T> for OneSize<T> {
 
     #[inline(always)]
     fn is_full(&self) -> bool {
-        let pos = self.pos.load(SeqCst);
-        let (head, tail) = Self::unpack(pos);
-        head < tail
+        !self.is_empty()
     }
 
     #[inline(always)]
@@ -171,19 +186,17 @@ impl<T> FlavorImpl<T> for OneSize<T> {
     #[inline(always)]
     fn try_send(&self, item: &MaybeUninit<T>) -> bool {
         // Will always double-check with is_full or try_send_oneshot()
-        let pos = self.pos.load(Relaxed);
-        unsafe { self.try_push(pos, item.as_ptr(), Relaxed).is_ok() }
+        unsafe { self._try_push(Relaxed, item.as_ptr(), Relaxed).is_ok() }
     }
 
     #[inline(always)]
     fn try_send_oneshot(&self, item: *const T) -> Option<bool> {
-        let pos = self.pos.load(SeqCst);
-        Some(unsafe { self.try_push(pos, item, SeqCst).is_ok() })
+        Some(unsafe { self._try_push(SeqCst, item, SeqCst).is_ok() })
     }
 
     #[inline(always)]
     fn try_recv(&self) -> Option<T> {
-        self.pop()
+        self._pop(SeqCst)
     }
 
     #[inline]
