4c3602b34b4bae0a206c1dff7fb6e5610673ce82
plan <frostyplanet@gmail.com>
2026-07-16 16:33:38 +0800

    test: Add test for MultiplexDyn

--- test-suite/src/test_select_blocking.rs
+++ test-suite/src/test_select_blocking.rs
@@ -1,9 +1,11 @@
 use crate::*;
 use captains_log::logfn;
-use crossfire::select::{Multiplex, Mux, Select};
+use crossfire::flavor::Queue;
+use crossfire::select::{Multiplex, MultiplexDyn, Mux, Select};
 use crossfire::*;
 use rstest::*;
 use std::collections::BTreeSet;
+use std::fmt::Debug;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Barrier};
 use std::thread;
@@ -1137,6 +1139,198 @@ fn test_pressure_multiplex_list(setup_log: (), #[case] producers: usize) {
 
 #[logfn]
 #[rstest]
+#[case(5, mpsc::bounded_blocking(10), mpsc::unbounded_blocking())]
+#[case(10, mpsc::bounded_blocking(100), mpsc::unbounded_blocking())]
+fn test_pressure_multiplex_dyn_mix_mpsc<T1, R1, T2, R2>(
+    setup_log: (), #[case] tx_count: usize, #[case] c1: (T1, R1), #[case] c2: (T2, R2),
+) where
+    T1: BlockingTxTrait<usize> + 'static + Send + Clone,
+    R1: ReceiverType + Debug + 'static + Send,
+    R1::Flavor: Queue<Item = usize>,
+    T2: BlockingTxTrait<usize> + Debug + 'static + Send + Clone,
+    R2: ReceiverType + Debug + 'static + Send,
+    R2::Flavor: Queue<Item = usize>,
+{
+    #[cfg(miri)]
+    {
+        if tx_count > 5 {
+            println!("skip");
+            return;
+        }
+    }
+    println!("run_test {:?} {:?}", c1, c2);
+    let (tx1, rx1) = c1;
+    let (tx2, rx2) = c2;
+    let round = ROUND;
+    let total_messages = round * 2 * tx_count;
+    let mut mux = MultiplexDyn::<usize>::new();
+    let mut handlers = Vec::new();
+    mux.add(rx1);
+    mux.add(rx2);
+    for _ in 0..tx_count {
+        let tx = tx1.clone();
+        handlers.push(thread::spawn(move || {
+            for i in 0..round {
+                tx.send(i).expect("send");
+            }
+        }));
+        let tx = tx2.clone();
+        handlers.push(thread::spawn(move || {
+            for i in 0..round {
+                tx.send(i).expect("send");
+            }
+        }));
+    }
+    drop(tx1);
+    drop(tx2);
+    let mut count = 0;
+    while count < total_messages {
+        match mux.recv() {
+            Ok(_) => count += 1,
+            Err(_) => break,
+        }
+    }
+    for h in handlers {
+        h.join().unwrap();
+    }
+    assert_eq!(count, total_messages);
+}
+
+#[logfn]
+#[rstest]
+#[case(5, 5, mpmc::bounded_blocking(10), mpmc::unbounded_blocking())]
+#[case(5, 10, mpmc::bounded_blocking(10), mpmc::unbounded_blocking())]
+#[case(10, 5, mpmc::bounded_blocking(100), mpmc::unbounded_blocking())]
+fn test_pressure_multiplex_dyn_mix_mpmc<T1, R1, T2, R2>(
+    setup_log: (), #[case] tx_count: usize, #[case] rx_count: usize, #[case] c1: (T1, R1),
+    #[case] c2: (T2, R2),
+) where
+    T1: BlockingTxTrait<usize> + 'static + Send + Clone,
+    R1: ReceiverType + Debug + 'static + Send + Clone,
+    R1::Flavor: Queue<Item = usize>,
+    T2: BlockingTxTrait<usize> + Debug + 'static + Send + Clone,
+    R2: ReceiverType + Debug + 'static + Send + Clone,
+    R2::Flavor: Queue<Item = usize>,
+{
+    #[cfg(miri)]
+    {
+        if tx_count > 5 || rx_count > 5 {
+            println!("skip");
+            return;
+        }
+    }
+    println!("run_test {:?} {:?}", c1, c2);
+    let (tx1, rx1) = c1;
+    let (tx2, rx2) = c2;
+    let round = ROUND;
+    let total_messages = round * 2 * tx_count;
+    let mut handlers = Vec::new();
+    for _ in 0..tx_count {
+        let tx = tx1.clone();
+        handlers.push(thread::spawn(move || {
+            for i in 0..round {
+                tx.send(i).expect("send");
+            }
+        }));
+        let tx = tx2.clone();
+        handlers.push(thread::spawn(move || {
+            for i in 0..round {
+                tx.send(i).expect("send");
+            }
+        }));
+    }
+    drop(tx1);
+    drop(tx2);
+    let mut rx_handlers = Vec::new();
+    for _ in 0..rx_count {
+        let mut mux = MultiplexDyn::<usize>::new();
+        mux.add(rx1.clone());
+        mux.add(rx2.clone());
+        rx_handlers.push(thread::spawn(move || {
+            let mut count = 0;
+            while count < total_messages {
+                match mux.recv() {
+                    Ok(_) => count += 1,
+                    Err(_) => break,
+                }
+            }
+            count
+        }));
+    }
+    drop(rx1);
+    drop(rx2);
+    for h in handlers {
+        h.join().unwrap();
+    }
+    let mut count = 0;
+    for h in rx_handlers {
+        count += h.join().unwrap();
+    }
+    assert_eq!(count, total_messages);
+}
+
+#[logfn]
+#[rstest]
+fn test_multiplex_dyn_sender_close_try_recv(setup_log: ()) {
+    const ITEMS: usize = 2; // per sender
+    let _deadline = Instant::now() + Duration::from_secs(10);
+    let mut iters = 0usize;
+    loop {
+        #[cfg(miri)]
+        {
+            if iters > 100 {
+                break;
+            }
+        }
+        #[cfg(not(miri))]
+        {
+            if Instant::now() > _deadline {
+                break;
+            }
+        }
+        let (tx1, rx1) = mpsc::bounded_blocking(10);
+        let (tx2, rx2) = mpsc::unbounded_blocking();
+        let mut th_s = Vec::new();
+        th_s.push(thread::spawn(move || {
+            for i in 0..ITEMS {
+                tx1.send(i).expect("send");
+            }
+        }));
+        th_s.push(thread::spawn(move || {
+            for i in 0..ITEMS {
+                tx2.send(i + ITEMS).expect("send");
+            }
+        }));
+        let mut mux = MultiplexDyn::<usize>::new();
+        mux.add(rx1);
+        mux.add(rx2);
+        let mut got = BTreeSet::new();
+        loop {
+            match mux.try_recv() {
+                Ok(_item) => {
+                    // NOTE: insert to btreeSet slows down the recv a bit, more effective to
+                    // re-produce than just increase a counter
+                    got.insert(_item);
+                }
+                Err(TryRecvError::Empty) => {
+                    if let Ok(_item) = mux.recv() {
+                        got.insert(_item);
+                    }
+                }
+                Err(TryRecvError::Disconnected) => break,
+            }
+        }
+        assert_eq!(got.len(), 2 * ITEMS, "lost message after {iters} iteration");
+        for th in th_s {
+            th.join().expect("join sender");
+        }
+        iters += 1;
+    }
+    println!("{iters} iterations");
+}
+
+#[logfn]
+#[rstest]
 fn test_multiplex_weighted_round_robin(setup_log: ()) {
     let mut mp = Multiplex::<mpsc::Array<i32>>::new();
     // Channel 1 with weight 2
@@ -1150,6 +1344,38 @@ fn test_multiplex_weighted_round_robin(setup_log: ()) {
         tx2.send(20 + i).unwrap(); // 20, 21, 22, 23, 24, 25
     }
 
+    let mut received = Vec::new();
+    for _ in 0..12 {
+        received.push(mp.recv().unwrap());
+    }
+
+    // Expected sequence:
+    // tx1 (10), tx1 (11), tx1 (12) -> weight exhausted (actually weight+1 logic), switch to tx2
+    // tx2 (20), tx2 (21), tx2 (22) -> weight exhausted, switch to tx1
+    // tx1 (13), tx1 (14), tx1 (15)
+    // tx2 (23), tx2 (24), tx2 (25)
+
+    let expected = vec![10, 11, 20, 21, 12, 13, 22, 23, 14, 15, 24, 25];
+
+    println!("Received: {:?}", received);
+    assert_eq!(received, expected);
+}
+
+#[logfn]
+#[rstest]
+fn test_multiplex_dyn_weighted_round_robin(setup_log: ()) {
+    let (tx1, rx1) = mpsc::bounded_blocking::<i32>(10);
+    let (tx2, rx2) = mpmc::bounded_blocking::<i32>(10);
+    let mut mp = MultiplexDyn::<i32>::new();
+    // Channel 1 with weight 2
+    mp.add_with_weight(rx1, 2);
+    // Channel 2 with weight 2
+    mp.add_with_weight(rx2, 2);
+    // Send data
+    for i in 0..6 {
+        tx1.send(10 + i).unwrap(); // 10, 11, 12, 13, 14, 15
+        tx2.send(20 + i).unwrap(); // 20, 21, 22, 23, 24, 25
+    }
     let mut received = Vec::new();
     for _ in 0..12 {
         received.push(mp.recv().unwrap());
@@ -1188,4 +1414,221 @@ fn test_multiplex_weighted_skip_empty(setup_log: ()) {
 
     // 3. Recv. tx2 is current (left=1).
     assert_eq!(mp.recv().unwrap(), 3);
+}
+
+#[logfn]
+#[rstest]
+fn test_multiple_dyn_mpmc_basic(setup_log: ()) {
+    // Create a multiplexer with bounded + unbounded channels
+    let (tx1, rx1) = mpmc::bounded_blocking::<i32>(10);
+    let (tx2, rx2) = mpmc::unbounded_blocking::<i32>();
+
+    let mut ths = Vec::new();
+    // Send values from different threads
+    ths.push(thread::spawn(move || {
+        for i in 0..10 {
+            tx1.send(i).expect("send");
+        }
+    }));
+    ths.push(thread::spawn(move || {
+        for i in 10..20 {
+            tx2.send(i).expect("send");
+        }
+    }));
+    for rx_id in 0..2 {
+        let mut mux = MultiplexDyn::<i32>::new();
+        mux.add(rx1.clone());
+        mux.add(rx2.clone());
+        ths.push(thread::spawn(move || {
+            while let Ok(msg) = mux.recv() {
+                println!("recv {msg}");
+            }
+            println!("{rx_id} exit");
+        }));
+    }
+    for th in ths {
+        th.join().expect("join");
+    }
+}
+
+#[logfn]
+#[rstest]
+fn test_multiplex_dyn_intermittent(setup_log: ()) {
+    let (tx1, rx1) = mpmc::bounded_blocking::<usize>(10);
+    let (tx2, rx2) = mpmc::unbounded_blocking::<usize>();
+
+    let mut mux = MultiplexDyn::<usize>::new();
+    mux.add(rx1);
+    mux.add(rx2);
+    const ROUND: usize = 100;
+
+    let th = thread::spawn(move || {
+        for i in 0..ROUND {
+            tx1.send(i).unwrap();
+            thread::sleep(Duration::from_millis(5));
+            tx2.send(ROUND + i).unwrap();
+            thread::sleep(Duration::from_millis(5));
+        }
+    });
+    let mut received = Vec::new();
+    while let Ok(msg) = mux.recv() {
+        received.push(msg);
+    }
+    th.join().unwrap();
+    received.sort();
+    let mut expected: Vec<usize> = Vec::with_capacity(ROUND * 2);
+    for i in 0..ROUND {
+        expected.push(i);
+    }
+    for i in 0..ROUND {
+        expected.push(i + ROUND);
+    }
+    assert_eq!(received, expected);
+}
+
+#[logfn]
+#[rstest]
+fn test_multiplex_dyn_sender_close_early_recv(setup_log: ()) {
+    let (tx1, rx1) = mpmc::bounded_blocking::<i32>(10);
+    let (tx2, rx2) = mpmc::bounded_blocking::<i32>(10);
+    let mut mux = MultiplexDyn::<i32>::new();
+    mux.add(rx1);
+    mux.add(rx2);
+    assert_eq!(mux.get_tx_count(), 2);
+    tx1.send(42).unwrap();
+    drop(tx1);
+    assert_eq!(mux.recv().unwrap(), 42);
+    drop(tx2);
+    // it's not aware until recv()
+    assert_eq!(mux.get_tx_count(), 2);
+    assert_eq!(mux.recv(), Err(RecvError));
+    assert_eq!(mux.get_tx_count(), 0);
+    // should not block or panic
+    assert_eq!(mux.recv(), Err(RecvError));
+}
+
+#[logfn]
+#[rstest]
+fn test_multiplex_dyn_sender_close_early_try_recv(setup_log: ()) {
+    let (tx1, rx1) = mpmc::bounded_blocking::<i32>(10);
+    let (tx2, rx2) = mpmc::bounded_blocking::<i32>(10);
+    let mut mux = MultiplexDyn::<i32>::new();
+    mux.add(rx1);
+    mux.add(rx2);
+    assert_eq!(mux.get_tx_count(), 2);
+
+    tx1.send(42).unwrap();
+    drop(tx1);
+
+    assert_eq!(mux.try_recv().unwrap(), 42);
+    drop(tx2);
+    // it's not aware until try_recv()
+    assert_eq!(mux.get_tx_count(), 2);
+    assert_eq!(mux.try_recv(), Err(TryRecvError::Disconnected));
+    assert_eq!(mux.get_tx_count(), 0);
+    // should not block or panic
+    assert_eq!(mux.try_recv(), Err(TryRecvError::Disconnected));
+}
+
+#[logfn]
+#[rstest]
+fn test_multiplex_dyn_concurrent_drop(setup_log: ()) {
+    let (tx1, rx1) = mpmc::bounded_blocking::<i32>(10);
+    let (tx2, rx2) = mpmc::unbounded_blocking::<i32>();
+    let rx1_clone1 = rx1.clone();
+    let rx1_clone2 = rx1;
+    let rx2_clone1 = rx2.clone();
+    let rx2_clone2 = rx2;
+
+    let th1 = thread::spawn(move || {
+        let mut mux1 = MultiplexDyn::<i32>::new();
+        mux1.add(rx1_clone1);
+        mux1.add(rx2_clone1);
+
+        let mut count1 = 0;
+        for _ in 0..5 {
+            if mux1.recv().is_ok() {
+                count1 += 1;
+            } else {
+                break;
+            }
+        }
+        count1
+    });
+
+    let th2 = thread::spawn(move || {
+        let mut mux2 = MultiplexDyn::<i32>::new();
+        mux2.add(rx1_clone2);
+        mux2.add(rx2_clone2);
+
+        let mut count2 = 0;
+        while let Ok(_) = mux2.recv() {
+            count2 += 1;
+        }
+        count2
+    });
+
+    let tx1_clone = tx1.clone();
+    let tx2_clone = tx2.clone();
+    let send_th1 = thread::spawn(move || {
+        for i in 0..15 {
+            let _ = tx1_clone.send(i);
+        }
+        drop(tx1_clone);
+    });
+    let send_th2 = thread::spawn(move || {
+        for i in 0..15 {
+            let _ = tx2_clone.send(100 + i);
+        }
+        drop(tx2_clone);
+    });
+
+    send_th1.join().unwrap();
+    send_th2.join().unwrap();
+    drop(tx1);
+    drop(tx2);
+
+    let count1 = th1.join().unwrap();
+    let count2 = th2.join().unwrap();
+
+    assert_eq!(count1 + count2, 30);
+}
+
+#[logfn]
+#[rstest]
+fn test_multiplex_dyn_remove_early(setup_log: ()) {
+    let (tx1, rx1) = mpmc::unbounded_blocking::<i32>();
+    let (tx2, rx2) = mpmc::bounded_blocking::<i32>(20);
+
+    let mut mux = MultiplexDyn::<i32>::new();
+    mux.add_with_weight(rx1, 2);
+    mux.add_with_weight(rx2.clone(), 2);
+
+    for i in 0..10 {
+        tx1.send(i).unwrap();
+        tx2.send(100 + i).unwrap();
+    }
+    let mut rx1_count = 0;
+    for _ in 0..4 {
+        if let Ok(msg) = mux.recv() {
+            if msg < 100 {
+                rx1_count += 1;
+            }
+        }
+    }
+    mux.remove(&rx2);
+    drop(rx2);
+    for i in 0..10 {
+        tx1.send(i).unwrap();
+    }
+    assert!(tx2.send(100).is_err());
+    drop(tx1);
+    while let Ok(msg) = mux.recv() {
+        if msg < 100 {
+            rx1_count += 1;
+        } else {
+            panic!("it's not expected to recv {msg} from removed handle");
+        }
+    }
+    assert_eq!(rx1_count, 20);
 }
